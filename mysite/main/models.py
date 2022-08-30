7b83f7b11ab67966e308c940e69e9bbe0974d62f
Jinyeong Heo <yeong2678@gmail.com>
2022-08-30 22:07:57 +0900

    feat: DB 모델링 및 세팅

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Oreum(models.Model):
+    name = models.CharField(max_length=50)
+    
+    locations = [
+        ('Susanbong', '수산봉'),
+        ('Biyangbong', '비양봉'),
+        ('Juge-oreum', '저지오름'),
+        ('Saebyeol-Oreum', '새별오름'), 
+        ('Eoseungsaengak', '어승생악'),
+        ('Ansemio', '안세미오'),
+        ('Geomun-Oreum', '거문오름'),
+        ('Manjang', '만장굴'),
+        ('Daranche-Oreum', '다랑쉬오름'),
+        ('Someori-Oreum', '소머리오름'),
+        ('Gunsan-Oreum', '군산오름'),
+        ('Moseulbong', '모슬봉'),
+    ]
+    
+    
+    location = models.CharField(max_length=50, choices=locations)
+    lat = models.FloatField(null =True)
+    lng = models.FloatField(null=True)
+    mainphoto = models.ImageField(blank=True, null=True)
+    subphoto = models.ImageField(blank=True, null=True)
+    published_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    phone = models.CharField(max_length=20)
+    insta = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.name
