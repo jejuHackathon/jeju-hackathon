3f1bfdc948768b63b3ed64a3bff1f174ccb87261
Muffin <jinlog9@gmail.com>
2022-08-31 11:30:18 +0900

    BE branch dev Brach merge
    * 1차 회의록 정리
    
    1차 회의록 정리
    
    * feat: 장고 초기 환경셋팅
    
    * [BE] DB 모델링 적용 및 세팅 (#8)
    
    * Update README.md
    
    * feat: 장고 초기 환경셋팅
    
    * feat: html 파일 생성 (#5)
    
    Co-authored-by: UngHeung <beehive2838@naver.com>
    
    * branch merge main to dev
    
    * 1차 회의록 정리
    
    1차 회의록 정리
    
    * Update README.md
    
    * feat: 장고 초기 환경셋팅
    
    * feat: html 파일 생성 (#5)
    
    Co-authored-by: UngHeung <beehive2838@naver.com>
    
    Co-authored-by: UngHeung <108179643+UngHeung@users.noreply.github.com>
    Co-authored-by: UngHeung <beehive2838@naver.com>
    
    * feat: DB 모델링 및 세팅
    
    Co-authored-by: Muffin <jinlog9@gmail.com>
    Co-authored-by: UngHeung <108179643+UngHeung@users.noreply.github.com>
    Co-authored-by: UngHeung <beehive2838@naver.com>
    
    * [BE] DB Table 수정 (#10)
    
    * Update README.md
    
    * feat: 장고 초기 환경셋팅
    
    * feat: html 파일 생성 (#5)
    
    Co-authored-by: UngHeung <beehive2838@naver.com>
    
    * branch merge main to dev
    
    * 1차 회의록 정리
    
    1차 회의록 정리
    
    * Update README.md
    
    * feat: 장고 초기 환경셋팅
    
    * feat: html 파일 생성 (#5)
    
    Co-authored-by: UngHeung <beehive2838@naver.com>
    
    Co-authored-by: UngHeung <108179643+UngHeung@users.noreply.github.com>
    Co-authored-by: UngHeung <beehive2838@naver.com>
    
    * feat: DB Table 수정
    
    Co-authored-by: Muffin <jinlog9@gmail.com>
    Co-authored-by: UngHeung <108179643+UngHeung@users.noreply.github.com>
    Co-authored-by: UngHeung <beehive2838@naver.com>
    
    Co-authored-by: Dev-Jinyeong <112479335+Dev-Jinyeong@users.noreply.github.com>
    Co-authored-by: UngHeung <108179643+UngHeung@users.noreply.github.com>
    Co-authored-by: UngHeung <beehive2838@naver.com>

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+class Oreum(models.Model):
+    name = models.CharField(max_length=50)
+    
+    locations = [
+        ('Hangyeong-myeon', '한경면'), 
+        ('Hallim-eup', '한림읍'),
+        ('Aewol-eup', '애월읍'), 
+        ('Jeju-si', '제주시'), 
+        ('Jocheon-eup', '조천읍'), 
+        ('Gujwa-eup', '구좌읍'),
+        ('Udo-myeon', '우도면'),
+        ('Seongsan-eup', '성산읍'),
+        ('Pyoseon-myeon', '표선면'),
+        ('Namwon-eup', '남원읍'),
+        ('Seogwipo-si', '서귀포시'),
+        ('Andeok-myeon', '안덕면'),
+        ('Daejeong-eup', '대정읍'),
+    ]
+    
+    oreumlevels = [
+        ('steep-slope', '경사 심함'),
+        ('easy-slope', '경사 완만'),
+    ]
+
+    nightviews = [
+        ('Good-view', '야경 좋음'),
+        ('normal-view', '야경 보통'),
+    ]
+
+    oreumlevel = models.CharField(max_length=30, choices=oreumlevels)
+    nightview = models.CharField(max_length=30, choices=nightviews)
+    location = models.CharField(max_length=50, choices=locations)
+    lat = models.FloatField(null =True)
+    lng = models.FloatField(null=True)
+    mainphoto = models.ImageField(blank=True, null=True)
+    subphoto = models.ImageField(blank=True, null=True)
+    published_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    
+    def __str__(self):
+        return self.name
+
--- mysite/tutorialdjango/settings.py
+++ mysite/tutorialdjango/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
@@ -123,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
