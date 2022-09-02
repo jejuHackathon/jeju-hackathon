bf8b63e83eea3a24ff263917817399b793888741
Muffin <jinlog9@gmail.com>
2022-09-02 15:03:52 +0900

    feat: 시작페이지 생성, 기존의 지도를 보여주는 index.html 페이지는 home.html 으로 라우팅

---
+++ mysite/main/static/js/startPage.js
@@ -0,0 +1,33 @@
+import { getItem, setItem } from './storage.js';
+
+export default function StartPage() {
+    const nickNameInput = document.querySelector('.oreum-start__nickname');
+    const learnButton = document.querySelector('.oreum-start__learn-button');
+    let nickName = '';
+
+    const init = () => {
+        nickNameInput?.addEventListener('change', handleOnChangeNickName);
+        learnButton?.addEventListener('click', handleOnClickLearnButton);
+        nickName = getItem('nickName', '');
+        if(nickName) {
+            location.href = '/home';
+        }
+    }
+
+    const handleOnChangeNickName = (e) => {
+        nickName = e.target.value;
+    }
+
+    const handleOnClickLearnButton = () => {
+        if(!nickName) {
+            alert('값을 입력해주세요!')
+            return;
+        }
+        setItem('nickName', nickName);
+        location.href = '/home';
+    }
+
+    init();
+}
+
+StartPage();
