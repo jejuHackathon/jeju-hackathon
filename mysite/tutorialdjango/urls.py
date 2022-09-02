8bd2431d0ac236dcb7ae8a2df5fa9674c0190e62
dlwlsgur0909 <74278791+dlwlsgur0909@users.noreply.github.com>
2022-09-02 20:21:23 +0900

    Be feature/update delete (#35)
    * 1차 회의록 정리
    
    1차 회의록 정리
    
    * Update README.md
    
    * feat: 장고 초기 환경셋팅
    
    * feat: html 파일 생성 (#5)
    
    Co-authored-by: UngHeung <beehive2838@naver.com>
    
    * feat: Create HTML & CSS 작업완료
    
    * feat: List 페이지 마크업 & CSS 완료
    
    * feat: Detail Page 마크업 진행, CSS 공통된 부분 common쪽으로 분리
    
    * [FE] FE 작업한 정적 페이지 dev 브랜치로 머지.
    
    * feat: Create HTML & CSS 작업완료
    
    * feat: List 페이지 마크업 & CSS 완료
    
    * feat: Detail Page 마크업 진행, CSS 공통된 부분 common쪽으로 분리
    
    * feat: main 페이지 마크업 & CSS
    
     main 페이지 마크업 & CSS
    
    * [BE] urls.py 및 view.py 임시 설정
    
    * 1차 회의록 정리
    
    1차 회의록 정리
    
    * Update README.md
    
    * feat: 장고 초기 환경셋팅
    
    * feat: html 파일 생성 (#5)
    
    Co-authored-by: UngHeung <beehive2838@naver.com>
    
    * [FE] FE 작업한 정적 페이지 dev 브랜치로 머지.
    
    * feat: Create HTML & CSS 작업완료
    
    * feat: List 페이지 마크업 & CSS 완료
    
    * feat: Detail Page 마크업 진행, CSS 공통된 부분 common쪽으로 분리
    
    * feat: views.py, urls.py 임시 설정
    
    Co-authored-by: Muffin <jinlog9@gmail.com>
    Co-authored-by: UngHeung <108179643+UngHeung@users.noreply.github.com>
    Co-authored-by: UngHeung <beehive2838@naver.com>
    
    Co-authored-by: Dev-Jinyeong <112479335+Dev-Jinyeong@users.noreply.github.com>
    Co-authored-by: UngHeung <108179643+UngHeung@users.noreply.github.com>
    Co-authored-by: UngHeung <beehive2838@naver.com>
    
    * FE branch merge to dev
    
    * feat: 하드코딩 => 데이터 교체 작업, 등록시 tags 기능 추가
    
    * feat: 오름 생성시 경사, 야경 라디오 버튼 추가, 오름 상세정보에서도 볼 수 있게 데이터 넣어주기, css 약간의 리팩토링
    
    * feat: 모달창 관련 기능 작업 진행
    
    * FE branch to dev
    
    * feat: 하드코딩 => 데이터 교체 작업, 등록시 tags 기능 추가
    
    * feat: 오름 생성시 경사, 야경 라디오 버튼 추가, 오름 상세정보에서도 볼 수 있게 데이터 넣어주기, css 약간의 리팩토링
    
    * feat: 모달창 관련 기능 작업 진행
    
    * feat: update & delete 기능구현
    
    Co-authored-by: Muffin <jinlog9@gmail.com>
    Co-authored-by: UngHeung <108179643+UngHeung@users.noreply.github.com>
    Co-authored-by: UngHeung <beehive2838@naver.com>
    Co-authored-by: Dev-Jinyeong <112479335+Dev-Jinyeong@users.noreply.github.com>

--- mysite/tutorialdjango/urls.py
+++ mysite/tutorialdjango/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from main.views import index, create, list, detail
+from main.views import index, create, list, detail, update, delete
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('delete/<int:pk>/', delete),
+    path('update/<int:pk>/', update),
     path('create/', create),
     path('list/', list),
-    path('detail/', detail),
+    path('list/<int:pk>/', detail),
     path('', index),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
