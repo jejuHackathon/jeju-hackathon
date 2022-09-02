2f840eecd3c8094888aba677dcd3b8633e36a942
Muffin <jinlog9@gmail.com>
2022-09-02 20:44:23 +0900

    Be branch to dev

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,14 +3,15 @@ from .models import Oreum
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    
+    # 닉네임을 입력 받으면 index 페이지로 이동 
+    if request.method == 'POST':
+        context = {
+            'locations' : Oreum.locations,
+        }
+        return render(request, 'main/index.html', context)
 
-def home(request):
-    context = {
-        'locations' : Oreum.locations
-    }
-    return render(request, 'main/home.html', context)
-
+    return render(request, 'main/nickname.html')
 
 def about(request):
     return render(request, 'main/about.html')
@@ -40,6 +41,8 @@ def detail(request, pk):
 
     context = {
         'oreum': oreum,
+        # context에 nickname값 추가 
+        'nickname': request.COOKIES.get('nickname'), 
     }
 
     return render(request, 'main/detail.html', context)
@@ -47,15 +50,18 @@ def detail(request, pk):
 
 def create(request):
     if request.method == 'POST':
-
         data = {
             'name': request.POST.get('name'),
+            'oreumlevel': request.POST.get('oreumlevel'),
+            'nightview' : request.POST.get('nightview'),
             'location': request.POST.get('location'),
             'phone': request.POST.get('phone'),
             'insta': request.POST.get('insta'),
             'content': request.POST.get('content'),
             'mainphoto': request.FILES.get('mainphoto'),
             'subphoto': request.FILES.get('subphoto'),
+            # cookie에 저장된 nickname값을 읽어와 nickname 컬럼에 저장 
+            'nickname': request.COOKIES.get('nickname')        
         }
 
         oreum = Oreum.objects.create(**data)
@@ -69,3 +75,42 @@ def create(request):
     }
 
     return render(request, 'main/create.html', context)
+
+
+def update(request, pk):
+  
+    if request.method == 'POST':
+        oreum = Oreum.objects.get(pk=pk)
+        oreum.name = request.POST.get('name')
+        oreum.oreumlevel = request.POST.get('oreumlevel')
+        oreum.nightview = request.POST.get('nightview')
+        oreum.location = request.POST.get('location')
+        oreum.phone = request.POST.get('phone')
+        oreum.insta = request.POST.get('insta')
+        oreum.content = request.POST.get('content')
+        # 새로 등록한 이미지가 없다면 변경하지 않음
+        if request.FILES.get('mainphoto'):
+            oreum.mainphoto = request.FILES.get('mainphoto')
+        if request.FILES.get('subphoto'):
+            oreum.subphoto = request.FILES.get('subphoto'),
+        oreum.save()
+
+        return redirect('/list')
+
+    oreum = Oreum.objects.get(pk=pk)
+  
+    context = {
+        'oreum': oreum,
+        'nickname': request.COOKIES.get('nickname'),
+        'locations': Oreum.locations,
+        'oreumlevels': Oreum.oreumlevels,
+        'nightviews': Oreum.nightviews, 
+    }
+    return render(request, "main/update.html", context)
+
+
+def delete(request, pk):
+    oreum = Oreum.objects.get(pk=pk)
+    oreum.delete()
+    return redirect('/list')
+
--- mysite/tutorialdjango/urls.py
+++ mysite/tutorialdjango/urls.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from main.views import index, home, about, create, list, detail
+from main.views import index, create, list, detail, update, delete
 from django.conf.urls.static import static
 from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('home/', home),
-    path('about/', about),
+    path('delete/<int:pk>/', delete),
+    path('update/<int:pk>/', update),
     path('create/', create),
     path('list/', list),
     path('list/<int:pk>/', detail),
+    path('', index),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
